04ff8342ca033415005393e853534ee3a8eb5cfa
Didi Ruhyadi <ruhyadi.dr@gmail.com>
2022-04-24 22:00:30 +0700

    develope change

--- src/calibration.py
+++ src/calibration.py
@@ -1,72 +1,98 @@
-import numpy as np
 import cv2
-import glob
+import numpy as np
+from utils import save_coefficients, save_kitti
+
 import argparse
-from calibration_store import save_coefficients
+import os
 
-# termination criteria
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+def calibrate_mono(
+    images_dir: str, 
+    columns: int, 
+    rows: int, 
+    square_size: int, 
+    save_type: str, 
+    output: str
+    ):
+    """
+    Calibrate monocular camera
+    ARGS:
+        image_dir: images directory
+        columns: num corners in columns of chessboard
+        rows: num corners in rows of chessboard
+        square_size: square size of chessboard pattern
+        save_type: type of mtx file (opencv, kitti)
+        output: output name
+    """
+    
+    # get images
+    imgs = os.listdir(images_dir)
 
-def calibrate(dirpath, prefix, image_format, square_size, width=9, height=6):
-    """ Apply camera calibration operation for images in the given directory path. """
     # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(8,6,0)
-    objp = np.zeros((height*width, 3), np.float32)
-    objp[:, :2] = np.mgrid[0:width, 0:height].T.reshape(-1, 2)
+    objp = np.zeros((columns*rows, 3), np.float32)
+    objp[:, :2] = np.mgrid[0:columns, 0:rows].T.reshape(-1, 2)
 
-    objp = objp * square_size  # Create real world coords. Use your metric.
+    objp = objp * square_size # create real world coords
 
     # Arrays to store object points and image points from all the images.
-    objpoints = []  # 3d point in real world space
-    imgpoints = []  # 2d points in image plane.
+    OBJPOINTS = []  # 3d point in real world space
+    IMGPOINTS = []  # 2d points in image plane.
 
-    # Directory path correction. Remove the last character if it is '/'
-    if dirpath[-1:] == '/':
-        dirpath = dirpath[:-1]
+    # termination criteria
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-    # Get the images
-    images = glob.glob(dirpath+'/' + prefix + '*.' + image_format)
+    for fname in imgs:
+        # read image in grayscale
+        img = cv2.imread(os.path.join(images_dir, fname), cv2.IMREAD_COLOR)
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
-    # Iterate through the pairs and find chessboard corners. Add them to arrays
-    # If openCV can't find the corners in an image, we discard the image.
-    for fname in images:
-        img = cv2.imread(fname)
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-        # Find the chess board corners
-        ret, corners = cv2.findChessboardCorners(gray, (width, height), None)
-
-        # If found, add object points, image points (after refining them)
-        if ret:
-            objpoints.append(objp)
+        # find chessboard pattern
+        ret, corners = cv2.findChessboardCorners(
+            gray, 
+            patternSize=(columns, rows), 
+            flags=cv2.CALIB_CB_ADAPTIVE_THRESH
+            )
+        
+        # continue if chessboard found on images
+        if ret==True:
+            # store objpoints
+            OBJPOINTS.append(objp)
 
             corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
-            imgpoints.append(corners2)
+            IMGPOINTS.append(corners2)
 
-            # Draw and display the corners
-            # Show the image to see if pattern is found ! imshow function.
-            img = cv2.drawChessboardCorners(img, (width, height), corners2, ret)
-            
-        cv2.imshow('calibration', img)
+            # draw corners on image
+            img_draw = cv2.drawChessboardCorners(img, (columns, rows), corners2, ret)
+
+        cv2.imshow('Monocular Calibration', img)
         cv2.waitKey(500)
 
-    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-
-    return [ret, mtx, dist, rvecs, tvecs]
+    # store matrix
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(OBJPOINTS, IMGPOINTS, gray.shape[::-1], None, None)
+    
+    # save matrix in file
+    if save_type=='opencv':
+        save_coefficients(mtx, dist, output)
+    elif save_type=='kitti':
+        save_kitti(mtx, dist, output)
+    else:
+        return [ret, mtx, dist, rvecs, tvecs] 
 
 if __name__ == '__main__':
     # Check the help parameters to understand arguments
     parser = argparse.ArgumentParser(description='Camera calibration')
-    parser.add_argument('--image_dir', type=str, required=True, help='image directory path')
-    parser.add_argument('--image_format', type=str, required=True,  help='image format, png/jpg')
-    parser.add_argument('--prefix', type=str, required=True, help='image prefix')
-    parser.add_argument('--square_size', type=float, required=False, help='chessboard square size')
-    parser.add_argument('--width', type=int, required=False, help='chessboard width size, default is 9')
-    parser.add_argument('--height', type=int, required=False, help='chessboard height size, default is 6')
-    parser.add_argument('--save_file', type=str, required=True, help='YML file to save calibration matrices')
+    parser.add_argument('--images_dir', type=str, help='image directory path')
+    parser.add_argument('--square_size', type=float, help='chessboard square size')
+    parser.add_argument('--columns', type=int, help='chessboard width size, default is 9')
+    parser.add_argument('--rows', type=int, help='chessboard height size, default is 6')
+    parser.add_argument('--save_type', type=str, help='Type of save file format [yaml, kitti(txt)]')
+    parser.add_argument('--output', type=str, help='Output file name with format')
 
     args = parser.parse_args()
 
-    # Call the calibraton and save as file. RMS is the error rate, it is better if rms is less than 0.2
-    ret, mtx, dist, rvecs, tvecs = calibrate(args.image_dir, args.prefix, args.image_format, args.square_size, args.width, args.height)
-    save_coefficients(mtx, dist, args.save_file)
-    print("Calibration is finished. RMS: ", ret)
+    res = calibrate_mono(
+        args.images_dir,
+        args.columns,
+        args.rows,
+        args.square_size,
+        args.save_type,
+        args.output)
--- src/runDocker.sh
+++
@@ -1,6 +0,0 @@
-xhost +local:docker
-XSOCK=/tmp/.X11-unix
-XAUTH=/tmp/.docker.xauth
-xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
-docker run -it --rm -e DISPLAY=$DISPLAY -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH -v ${PWD}:/calibration -it ruhyadi/calibration:latest
-xhost -local:docker
